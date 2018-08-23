d34c7c5b4638b15193e42ec8cc10a79a0c7525d2
dulingbo <dulingbo@nibirutech.com>
2018-08-23 16:24:20 +0800

    add service name and port for services

---
+++ config/api.js
@@ -0,0 +1,37 @@
+/**
+ * 各微服务的name和port
+ */
+module.exports = {
+    //考核方案
+    judgePlan:'judge_plans:3029',
+
+    //员工okr
+    employeeOkr:'employee_okrs:3023',
+
+    //部门okr
+    departmentOkr:'department_okrs:3025',
+    
+    //员工评价
+    employeeJudge:'employee_judgements:3021',
+
+    //部门评价
+    departmentJudge:'department_judgements:3022',
+
+    //周期服务
+    period:'periods:3014',
+
+    //附件服务
+    attachments:'attachments:3028',
+
+    //uac
+    uac:'uac_bonus',
+
+    //薪人薪事访问
+    xrxs:'xrxs-service:3016',
+
+    //基础员工信息服务
+    basicEmployees:'employee-basic-service-bonus:3027',
+
+    //基础部门信息服务
+    basicDepartments:'department-basic-service-bonus:3026'
+}
