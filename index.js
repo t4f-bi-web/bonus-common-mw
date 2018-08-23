caaea3c7f35640a4ee0b1334a15d1c93cfcac1c7
dulingbo <dulingbo@nibirutech.com>
2018-08-23 16:42:57 +0800

    add a export to all the api in bonus service

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 const CommonMiddleware = require('./lib/commonMiddleware');
+const API = require('./config/api');
 
-module.exports = CommonMiddleware;
+module.exports = {
+    CommonMiddleware,
+    API
+}
