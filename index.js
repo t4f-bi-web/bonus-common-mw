bbf0a16b64324402c576e197fdd6b40d2afbc686
dulingbo <dulingbo@nibirutech.com>
2018-08-28 11:05:14 +0800

    add a new module, to register a service to erueka

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const CommonMiddleware = require('./lib/commonMiddleware');
 const { API,services } = require('./config/serives');
+const RegisterEureka = require('./lib/registerEureka')
 
 module.exports = {
     CommonMiddleware,
+    RegisterEureka,
     API,
     services
 }
