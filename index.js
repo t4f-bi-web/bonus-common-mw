8cae266409695a3339037b285efedb57be22f338
duhuiping <duhuiping@nibirutech.com>
2018-09-03 10:58:16 +0800

    add User.roles function

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const CommonMiddleware = require('./lib/commonMiddleware');
 const { API,services } = require('./config/serives');
-const EurekaClient = require('./lib/registerEureka')
+const EurekaClient = require('./lib/registerEureka');
+const User = require('./lib/user');
 
 module.exports = {
     CommonMiddleware,
     EurekaClient,
+    User,
     API,
     services
 }
